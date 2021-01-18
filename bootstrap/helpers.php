a37807a2d3898681e622855b2f5d1d51a7619ea8
breadthe <omigoshdev@protonmail.com>
2021-01-18 00:41:51 -0600

    Helpers

---
+++ bootstrap/helpers.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @see https://stackoverflow.com/questions/2510434/format-bytes-to-kilobytes-megabytes-gigabytes
+ */
+function formatBytes(int $bytes, $precision = 2)
+{
+    $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+    $bytes = max($bytes, 0);
+    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+    $pow = min($pow, count($units) - 1);
+
+    // Uncomment one of the following alternatives
+    // $bytes /= pow(1024, $pow);
+    $bytes /= (1 << (10 * $pow));
+
+    return round($bytes, $precision) . ' ' . $units[$pow];
+}
