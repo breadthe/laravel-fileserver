6ddda74b5babd4391f889b69fd00d623026fb670
breadthe <omigoshdev@protonmail.com>
2021-01-20 22:20:29 -0600

    Change to daily logging

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'], // 'single', 'expired-secrets'
             'ignore_exceptions' => false,
         ],
 
