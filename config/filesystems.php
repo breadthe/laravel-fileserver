53d1c21a50cf76b8df851548d37b67107a2ef3f3
breadthe <omigoshdev@protonmail.com>
2021-01-25 18:08:20 -0600

    Add test disk to config

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,6 +35,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'tmp-for-tests' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
